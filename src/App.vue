6d6ef19183b5e35b3a893ce1f6eb8caa373fec97
DESKTOP-RN8B1C2\BxThanhLove <thanhdv.93@gmail.com>
2018-10-06 16:28:00 +0700

    add Component feature

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <app-feature></app-feature>
     <app-user></app-user>
     <app-registration></app-registration>
     <app-registrations></app-registrations>
@@ -10,12 +11,14 @@
   import Registration from './components/Registration.vue';
   import Registrations from './components/Registrations.vue';
   import User from './components/User.vue';
+  import Feature from './components/Feature.vue';
 
 export default {
     components: {
         appRegistration: Registration,
         appRegistrations: Registrations,
         appUser:User,
+        appFeature:Feature,
     }
 }
 </script>
