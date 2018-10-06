10cea17c1bc58f64f48f670f9f75f992e992dd99
DESKTOP-RN8B1C2\BxThanhLove <thanhdv.93@gmail.com>
2018-10-06 16:31:18 +0700

    feature

---
+++ src/components/Feature.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <h2>Component feature</h2>
+        <p class="p-welcome">
+            Welcome to component {{data}}
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            data: 'Đỗ Thanh'
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .p-welcome {
+        font-size: 16px;
+        font-weight: 700;
+        box-shadow: 1px;
+    }
+</style>
+
+
