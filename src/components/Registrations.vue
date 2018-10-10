01162f5a46d071dfbf2efa6fd806378c445bf5c1
Đỗ Thanh <thanhdv.93@gmail.com>
2018-10-10 16:10:26 +0700

    modifed 2 file

--- src/components/Registrations.vue
+++ src/components/Registrations.vue
@@ -31,6 +31,7 @@ export default {
             total: 'totalRegistrations'
         })
 
+        //Đã sửa ở develop
         // registrations(){
         //     return this.$store.getters.registrations;
         // },
