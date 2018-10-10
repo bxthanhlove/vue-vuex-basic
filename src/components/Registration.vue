fac2bcc52d3beeb67fbf5d4ff8b5e202ca405e2f
Đỗ Thanh <thanhdv.93@gmail.com>
2018-10-10 16:01:10 +0700

    commit in master

--- src/components/Registration.vue
+++ src/components/Registration.vue
@@ -19,11 +19,7 @@
         methods: {
             registerUser(user) {
                 this.$store.commit('register', user);
-<<<<<<< HEAD
-                //this.$store.dispatch('register', user); //sửa code branch develop in master 1
-=======
-                //this.$store.dispatch('register', user); //sửa code branch develop aaaa
->>>>>>> modified in develop
+                //this.$store.dispatch('register', user); //sửa code branch develop in master 1 aaa
             }
         }
     }
